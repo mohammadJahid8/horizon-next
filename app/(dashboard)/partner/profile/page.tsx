de675b1922d7bedf2a1090fe605a281d4d15af88
mohammadJahid8 <mohammadjahid0007@gmail.com>
2024-12-23 23:51:58 +0600

    latest

--- app/(dashboard)/partner/profile/page.tsx
+++ app/(dashboard)/partner/profile/page.tsx
@@ -1,4 +1,3 @@
-import PartnerCompanyInformation from '@/components/global/dashboard/partner-company-information';
 import PartnerPersonalInformation from '@/components/global/dashboard/partner-personal-information';
 import React from 'react';
 
@@ -6,7 +5,6 @@ const PartnerProfile = () => {
   return (
     <div className='flex flex-col gap-8'>
       <PartnerPersonalInformation />
-      <PartnerCompanyInformation />
     </div>
   );
 };
