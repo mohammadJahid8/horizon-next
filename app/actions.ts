1700659db49c6ae5dadd8320342025fe0a9e9f54
mohammadJahid8 <mohammadjahid0007@gmail.com>
2025-03-29 14:47:32 +0600

    latest

--- app/actions.ts
+++ app/actions.ts
@@ -1,11 +1,11 @@
-'use server';
-import api from '@/lib/axiosInterceptor';
-import { client } from '@/sanity/lib/client';
-import { cookies } from 'next/headers';
+"use server";
+import api from "@/lib/axiosInterceptor";
+import { client } from "@/sanity/lib/client";
+import { cookies } from "next/headers";
 export async function getUser() {
   try {
     const response = await api.get(`/user/profile`);
-    // console.log('response', response.data);
+
     return response.data.data;
   } catch (error) {
     // console.error('Error fetching user profile:', error);
@@ -60,16 +60,16 @@ export async function getNotifications() {
 }
 
 export async function getTokens() {
-  const accessToken = cookies().get('accessToken')?.value;
-  const refreshToken = cookies().get('refreshToken')?.value;
-  const tokenRefreshIn = cookies().get('tokenRefreshIn')?.value;
+  const accessToken = cookies().get("accessToken")?.value;
+  const refreshToken = cookies().get("refreshToken")?.value;
+  const tokenRefreshIn = cookies().get("tokenRefreshIn")?.value;
   return { accessToken, refreshToken, tokenRefreshIn };
 }
 
 export async function logout() {
-  cookies().delete('accessToken');
-  cookies().delete('refreshToken');
-  cookies().delete('tokenRefreshIn');
+  cookies().delete("accessToken");
+  cookies().delete("refreshToken");
+  cookies().delete("tokenRefreshIn");
 }
 
 export async function getEnvironment() {
